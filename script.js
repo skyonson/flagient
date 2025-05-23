1b907afc219c8db8ab6cf651d65469f8c99d7b62
Alex G <49047845+skyonson@users.noreply.github.com>
2025-05-23 03:59:07 +0000

    creating flag building logic

--- script.js
+++ script.js
@@ -1,16 +1,76 @@
 document.querySelector("#flag").onclick = copy;
 
 let flags = {
-    trans:["#5BCFFB",
+    trans: [
+        "#5BCFFB",
         "#F5ABB9",
         "#FFFFFF",
         "#F5ABB9",
         "#5BCFFB"
+    ],
+    lesbian: [
+        "#D62800",
+        "#FF9B56",
+        "#FFFFFF",
+        "#D462A6",
+        "#A40062"
+    ],
+    bisexual: [
+        "#D60270",
+        "#D60270",
+        "#9B4F96",
+        "#0038A8",
+        "#0038A8"
+    ],
+    aromantic: [
+        "#3BA740",
+        "#A8D47A",
+        "#FFFFFF",
+        "#ABABAB",
+        "#000000"
+    ],
+    genderfluid: [
+        "#FE76A2",
+        "#FFFFFF",
+        "#BF12D7",
+        "#000000",
+        "#303CBE"
     ]
 }
+
+
+let flag1 = flags.trans;
+let flag2 = flags.lesbian;
+
 
 function copy() {
     html2canvas(document.querySelector("#capture")).then(canvas => {
         canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })]))
     });
+}
+
+function buildFlag(leftFlag, rightFlag) {
+            document.querySelector("#flag").innerHTML = ""
+    for (let i = 0; i < lcm(leftFlag.length, rightFlag.length); i++) {
+        let stripe = document.createElement("div")
+        stripe.style.background = `linear-gradient(in oklab 90deg, ${leftFlag[i / leftFlag.length]} 20%, ${rightFlag[i / rightFlag.length]} 80%);`
+        document.querySelector("#flag").appendChild(stripe)
+    }
+}
+
+buildFlag(flag1, flag2);
+
+
+function gcd(a, b) {
+    for (let temp = b; b !== 0;) {
+        b = a % b;
+        a = temp;
+        temp = b;
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    const gcdValue = gcd(a, b);
+    return (a * b) / gcdValue;
 }
